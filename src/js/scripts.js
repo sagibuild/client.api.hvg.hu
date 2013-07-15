e2bd0ab8603fe3a30b49e9b3cac9f2b488a2c2b0
Richard Kundl <kundl.richard@gmail.com>
2013-07-15 21:43:18 +0200

    testing page create from json

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -17,10 +17,10 @@ $('#btn-rovat').bind('click', function(event, ui) {
 // Render article body
 // TODO: Make a handlebarsjs remplate
 function renderArticle(article){
-	var caption = '<h3><a href="' + siteArticleUrl(article.Column.WebId, article.WebId) + '" title="' + article.Caption + '">' + article.Caption + '</a></h3>';
+	var caption = '<h3><a href="' + siteArticleUrl(article.Column.WebId, article.WebId) + '" data-transition="pop" title="' + article.Caption + '">' + article.Caption + '</a></h3>';
 	var image = '';
 	if (article.DefaultImageId != '00000000-0000-0000-0000-000000000000'){
-		image = '<a href="' + siteArticleUrl(article.Column.WebId, article.WebId) + '" title="' + article.Caption + '">'
+		image = '<a href="' + siteArticleUrl(article.Column.WebId, article.WebId) + '" data-transition="pop" title="' + article.Caption + '">'
 				+ '<img class="framed" src="http://img8.hvg.hu/image.aspx?id='+article.DefaultImageId+'&amp;view=a7ce225f-67ef-4b6d-b77d-59581e02f304" align="left">'
 				+ '</a>';
 	}
@@ -28,7 +28,7 @@ function renderArticle(article){
 	var lead = '<p>'+article.Lead+'</p>';
 	var info = '<p class="columnarticleinfo">'
 				+ '<img class="transparent" src="http://img5.hvg.hu/static/skins/default/img/new-icons/time.png" alt="ido">'+ localDateTime(article.ReleaseDate) 
-				+ '<a href="' + siteArticleUrl(article.Column.WebId, article.WebId) + '" title="' + article.Caption + '"><img class="transparent" alt="szerzo" src="http://img9.hvg.hu/static/skins/default/img/new-icons/author.png">MTI</a>'
+				+ '<a href="' + siteArticleUrl(article.Column.WebId, article.WebId) + '" data-transition="pop" title="' + article.Caption + '"><img class="transparent" alt="szerzo" src="http://img9.hvg.hu/static/skins/default/img/new-icons/author.png">MTI</a>'
 				+ '</p>';
 
 	var ret = '<div class="article">'+ caption + image + lead + info + '</div>';
